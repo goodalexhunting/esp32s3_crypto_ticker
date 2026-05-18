5722030e6f5fafcf595ab2d67648725f031eb440
Alex San <goodalexhunting@gmail.com>
2026-05-19 00:19:20 +0100

    Refactor layout and crypto logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,41 @@
  *
  */
 
+#include <HTTPClient.h>
+#include <layout_manager.h>
 #include <lgfx_user_setup.h>
-
-#include "HTTPClient.h"
-#include "layout_manager.h"
-#include "wifi_mgr.h"
-
+#include <wifi_mgr.h>
 
 #define UPDATE_INTERVAL 1000 * 60 * 1
 
 extern void            update_crypto();
 extern void            render_layout(LovyanGFX& tft);
+extern void            configure_tft();
 unsigned long          last_update = 0L;
 Rect                   btcRect;
 Rect                   suiRect;
 Rect                   solRect;
+Rect                   footer;
+Rect                   header;
+Rect                   content;
 cryptoapp::WifiManager wifi;
 LGFX                   tft;
 
 void setup() {
-    Serial.println("BOOT 1");
+    Serial.println("SETUP 1: Serial starting");
     Serial.begin(115200);
     tft.setBrightness(150);
-    Serial.println("BOOT 2: WiFi starting");
+    Serial.println("SETUP 2: WiFi starting");
     wifi.connectWifi();
     // TODO: when this fails, we should not continue with the rest of the setup, but instead show an
     // error message on the display and retry connecting to WiFi after some time
 
-    Serial.println("BOOT 3: TFT Config and first update");
+    Serial.println("SETUP 3: TFT Config and first update");
+    configure_tft();
+    update_crypto();
+}
+
+void configure_tft() {
     tft.init();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
@@ -42,42 +49,44 @@ void setup() {
     tft.setTextWrap(true);
     tft.setCursor(0, 0);
     tft.setTextSize(3);
-    update_crypto();
+}
+
+void draw_header() {
+    tft.fillRect(header.x, header.y, header.w, header.h, TFT_BLACK);
+    tft.setCursor(header.x + 10, header.y + 10);
+    tft.print("ESP32S3 Crypto Ticker");
+}
+
+void draw_footer() {
+    tft.fillRect(footer.x, footer.y, footer.w, footer.h, TFT_BLACK);
+    tft.setCursor(footer.x + 10, footer.y + 10);
+    tft.print("By github.com/goodalexhunting");
 }
 
 void render_layout(LovyanGFX& tft) {
     LayoutManager layout = LayoutManager(tft.width(), tft.height());
 
-    Rect          header  = layout.grid(1, 6, 0, 0, 1, 1);
-    Rect          content = layout.grid(1, 6, 0, 1, 1, 4);
-    Rect          footer  = layout.grid(1, 6, 0, 5, 1, 1);
+    header  = layout.grid(1, 6, 0, 0, 1, 1);
+    content = layout.grid(1, 6, 0, 1, 1, 4);
+    footer  = layout.grid(1, 6, 0, 5, 1, 1);
     LayoutManager contentLayout(content.w, content.h);
 
     btcRect = contentLayout.inset(contentLayout.grid(1, 3, 0, 0), 0);
     suiRect = contentLayout.inset(contentLayout.grid(1, 3, 0, 1), 0);
     solRect = contentLayout.inset(contentLayout.grid(1, 3, 0, 2), 0);
 
-    // Offset by content.x and content.y
     btcRect.x += content.x;
     btcRect.y += content.y;
+
     suiRect.x += content.x;
     suiRect.y += content.y;
+
     solRect.x += content.x;
     solRect.y += content.y;
 
-    // Draw header
-    tft.fillRect(header.x, header.y, header.w, header.h, TFT_BLACK);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.setCursor(header.x + 10, header.y + 10);
-    tft.print("ESP32S3 Crypto Ticker");
-
-    // Draw footer
-    tft.fillRect(footer.x, footer.y, footer.w, footer.h, TFT_BLACK);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.setCursor(footer.x + 10, footer.y + 10);
-    tft.setTextSize(1);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.print("By github.com/goodalexhunting");
+    // Draw header & footer
+    draw_header();
+    draw_footer();
 }
 
 void loop() {
